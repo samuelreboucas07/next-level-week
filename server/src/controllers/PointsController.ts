729f715103d7a92966bfcb514b437055c48489dd
Samuel Rebouças de Jesus <samuelreboucas07@hotmail.com>
2020-07-11 22:16:21 -0300

    Continuação backend

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,7 +4,10 @@ import {Request, Response} from 'express';
 class PointsController {
     async createPoint(req: Request, res: Response){
         const  {name, email, whatsapp, latitude, longitude, city, uf, items} = req.body;
-        const points = await knex('points').insert({
+
+        const trx = await knex.transaction(); // Responsável por fazer o rollback dos inserts, descarta o que foi feito caso dê algum erro.
+
+        const point  = {
             image: 'image-fake', 
             name, 
             email, 
@@ -13,18 +16,40 @@ class PointsController {
             longitude, 
             city, 
             uf
-        });
+        };
+
+        const pointsInserted = await trx('points').insert(point);
+        const point_id = pointsInserted[0];
+
         const pointItems = items.map((item_id: number) => {
             return{
                 item_id,
-                point_id: points[0] 
+                point_id 
             }
         })
 
-        await knex('point_items').insert(pointItems);
-
-        return res.json({success: true})
+        await trx('point_items').insert(pointItems);
+        //ERRO ROLLBACK
+        await trx.commit();
+        return res.json({id: point_id, ...point})
     }
+
+    async show(req: Request, res: Response){
+        const {id} = req.params;
+        const point = await knex('points').where('id', id).first();
+        
+    if(!point){
+        return res.status(400).json({message: 'Point not found.'})
+    }
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.title');
+
+        return  res.json({point, items});
+    }
+
+
 }
 
 export default new PointsController();
